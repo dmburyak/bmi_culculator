09719877d2512d416444c22f8a725be86841bd17
dmitri <d.buryak@gmail.com>
2022-01-05 22:44:35 +0200

    add containers for buttons

---
+++ lib/input_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFF0A0E21),
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(color: Colors.blueAccent),
+                  ),
+                  Expanded(
+                    child: ReusableCard(),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ReusableCard(),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(),
+                  ),
+                  Expanded(
+                    child: ReusableCard(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({this.color = const Color(0xFF1D1E33)});
+
+ final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
