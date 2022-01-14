487e18310494ed7704f02cb16920538b7063ab9a
dmitri <d.buryak@gmail.com>
2022-01-14 23:04:41 +0200

    change slider

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -81,17 +81,25 @@ class _InputPageState extends State<InputPage> {
                           SizedBox(width: 10),
                           Text('cm', style: kCardTextStyle),
                         ]),
-                    Slider(
-                        value: height.toDouble(),
-                        min: 120.0,
-                        max: 220.0,
-                        activeColor: Color(0xFFEB1555),
-                        inactiveColor: Color(0xFF8D8E98),
-                        onChanged: (double newValue) {
-                          setState(() {
-                            height = newValue.round();
-                          });
-                        })
+                    SliderTheme(
+                      data: SliderTheme.of(context).copyWith(
+                        thumbColor: Colors.pink,
+                        overlayColor: Color(0x29EB1555),
+                        activeTrackColor: Colors.white,
+                        inactiveTrackColor: Color(0xFF8D8E98),
+                        thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                        overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
+                      ),
+                      child: Slider(
+                          value: height.toDouble(),
+                          min: 120.0,
+                          max: 220.0,
+                          onChanged: (double newValue) {
+                            setState(() {
+                              height = newValue.round();
+                            });
+                          }),
+                    )
                   ],
                 ),
               ),
